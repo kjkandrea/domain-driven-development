e23d4a8f206ac9c57bafe39b7c9a7865102f810c
kjkandrea <kjkandrea@gmail.com>
2023-02-08 12:58:41 +0900

    feat: UserService 구현

---
+++ src/user/services/UserService.ts
@@ -0,0 +1,14 @@
+import {IUserRepository} from "user/repositories/UserRepository";
+import {User} from "user/entities/User";
+
+export class UserService {
+  private userRepository: IUserRepository;
+
+  constructor(userRepository: IUserRepository) {
+    this.userRepository = userRepository;
+  }
+
+  public exists(user: User): Promise<boolean> {
+    return this.userRepository.exists(user)
+  }
+}
