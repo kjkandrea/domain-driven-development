af993957ae8fba9b4e175554f6228187b3399ddd
kjkandrea <kjkandrea@gmail.com>
2023-02-09 15:24:27 +0900

    test: UserApplicationService.update 테스트

--- src/UserApplicationService.test.ts
+++ src/UserApplicationService.test.ts
@@ -13,6 +13,7 @@ describe('UserApplicationService', () => {
   );
 
   const USER_NAME_VALUE = 'karenin';
+  const USER_ID = 1;
 
   test('유저를 생성한 후 해당 유저를 레포지토리에서 읽을 수 있다.', async () => {
     await userApplicationService.register(USER_NAME_VALUE);
@@ -23,13 +24,27 @@ describe('UserApplicationService', () => {
   });
 
   test('id 를 통해 저장된 유저 정보를 조회할 수 있다.', async () => {
-    const USER_ID = 1;
-
     const user = new User(new UserName(USER_NAME_VALUE), new UserId(USER_ID));
 
     await userRepository.save(user);
     await expect(userApplicationService.get(USER_ID)).resolves.toEqual(
       user.getValues()
+    );
+  });
+
+  test('id 를 통해 저장된 유저의 정보를 수정할 수 있다.', async () => {
+    const user = new User(new UserName(USER_NAME_VALUE), new UserId(USER_ID));
+
+    await userRepository.save(user);
+
+    const NEW_USER_NAME_VALUE = 'kareninano';
+    const NEW_USER_DATA = {
+      id: USER_ID,
+      name: NEW_USER_NAME_VALUE,
+    };
+    await userApplicationService.update(NEW_USER_DATA);
+    await expect(userApplicationService.get(USER_ID)).resolves.toEqual(
+      NEW_USER_DATA
     );
   });
 });
