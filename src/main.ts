4404a546435b890a5ef1fb1435bfdaa8fe6423d1
kjkandrea <kjkandrea@gmail.com>
2023-02-09 16:22:04 +0900

    feat: UserApplicationService delete 사용

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import {UserRepository} from 'user/repositories/UserRepository';
 import {UserService} from 'user/services/UserService';
 import {UserApplicationService} from 'UserApplicationService';
-import {UserId} from 'user/values';
 
 async function main() {
   const userRepository = new UserRepository();
@@ -22,12 +21,7 @@ async function main() {
     console.log('에러 무시 : 유저 이름 안바뀌어도 괜찮아..');
   }
   program.get(1).then(console.log);
-  const deleteTarget = await userRepository.findById(new UserId(2));
-  if (deleteTarget) {
-    userRepository
-      .delete(deleteTarget)
-      .then(() => console.log('id 2 user deleted'));
-  }
+  program.delete(3).then(() => console.log('id 3 user deleted'));
 }
 
 main();
