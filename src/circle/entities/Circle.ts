dcdc5e81c4d786cb7dff37d9abdd7152c43f2a32
kjkandrea <kjkandrea@gmail.com>
2023-02-17 09:46:20 +0900

    feat: Circle 엔티티 추가

---
+++ src/circle/entities/Circle.ts
@@ -0,0 +1,22 @@
+import {CircleName} from 'circle/values/CircleName';
+import {CircleId} from 'circle/values/CircleId';
+import {User} from 'user/entities/User';
+
+export class Circle {
+  private circleName: CircleName;
+  private readonly circleId: CircleId;
+  private owner: User;
+  private members: User[];
+
+  constructor(
+    circleName: CircleName,
+    circleId: CircleId,
+    owner: User,
+    members: User[]
+  ) {
+    this.circleName = circleName;
+    this.circleId = circleId;
+    this.owner = owner;
+    this.members = members;
+  }
+}
